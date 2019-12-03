ffe22691f8410a552d93e24224f4c0897a3a03f0
3A632011 <abc4173727@gmail.com>
2019-12-03 22:17:41 +0800

    練習 3：測試 View 上有無錯誤訊息

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Post;
 use App\Http\Requests\PostRequest;
+
 class AdminPostsController extends Controller
 {
     public function index()
