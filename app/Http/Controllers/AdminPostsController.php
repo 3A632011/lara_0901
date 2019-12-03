c2b75b02de83c4cf0b4db5a6597999df6f07f768
3A632011 <abc4173727@gmail.com>
2019-12-03 21:38:40 +0800

    練習6-1:修改AdminPostsControlle

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -23,10 +23,11 @@ class AdminPostsController extends Controller
 
     public function edit($id)
     {
-        $data = ['id' => $id];
-
-        return view('admin.posts.edit', $data);
+        $post=Post::find($id);
+        $data=['post'=>$post];
+        return view('admin.posts.edit',$data);
     }
+
     public function store(Request$request)
     {
         Post::create($request->all());
